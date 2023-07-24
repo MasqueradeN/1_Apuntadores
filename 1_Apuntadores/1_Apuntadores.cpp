c72f123cfb082ab9d7d3aa88e1e8b7cdd0574676
Masquerade N <pisaacrc52@gmail.com>
2023-07-24 15:24:41 -0600

    Implementación de Promedio con cin y apuntadores de funciones

--- 1_Apuntadores/1_Apuntadores.cpp
+++ 1_Apuntadores/1_Apuntadores.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
 using namespace std;
-
-int main(int argc, char** argv)
+void PruebaDeArgumentos(int argc, char** argv)
 {
     cout << "Da numero de argumentos brou: \n";
     cin >> argc;
@@ -28,7 +27,100 @@ int main(int argc, char** argv)
     }
     else
     {
-        cout << "Brubruvbruv. See ya nerds! xdxd\n";
+        cout << "Bruhbruvbruv. See ya nerds! xdxd\n";
     }
+}
+
+void Promedio(int argc, char** argv)
+{
+    int* x = new int;//(int*)malloc(sizeof(int));
+    *x = 99;
+
+    cout << "sizeof int " << sizeof(int) << "\n";
+    cout << " x " << *x << "\n";
+
+    *x = 'k';
+
+    cout << "sizeof int " << sizeof(int) << "\n";
+    cout << " x " << *x << " en " << x << "\n";
+
+    *(x + 1) = 18;
+
+    cout << "sizeof int " << sizeof(int) << "\n";
+    cout << " x+1 " << *(x + 1) << " en " << x + 1 << "\n";
+
+    int* y = new int[214748369];
+    char continuar;
+    bool puedeContinuar = true;
+    memset(y, 0, sizeof(int));
+
+    int* acumulador = new int;
+    *acumulador = 0;
+    float div;
+    div = 0;
+
+
+    for (int i = 0; i < 214748369; i++)
+    {
+        if (puedeContinuar)
+        {
+            div += 1;
+            cout << "Introduce los valores para el promedio\n";
+            cin >> y[i];
+            cout << " y[ " << i << " ] = " << y[i] << "\n";
+            *acumulador += y[i];
+        }
+        if (puedeContinuar || puedeContinuar == false)
+        {
+            cout << "Quieres continuar? Presiona S/N: ";
+            cin >> continuar;
+        }
+        if (continuar == 'S' || continuar == 's')
+        {
+            puedeContinuar = true;
+            continue;
+        }
+        else  if (continuar == 'N' || continuar == 'n')
+        {
+            break;
+        }
+        else
+        {
+            cout << "Letra erronea\n";
+            i -= 1;
+            puedeContinuar = false;
+        }
+    }
+    float promedio = *acumulador / div;
+    cout << " Promedio = " << promedio << "\n";
+}
+
+int main(int argc, char** argv)
+{
+    void (*proceso)(int, char**) = nullptr;
+
+    cout << "Funciones disponibles: \n";
+    cout << "   1. Prubea de argumentos. \n";
+    cout << "   2. Promedio. \n";
+    cout << "Ingresa el numero de la funcion deseada: ";
+    int seleccion;
+    cin >> seleccion;
+
+    if (seleccion == 1)
+    {
+        proceso = &PruebaDeArgumentos;
+        (*proceso)(argc, argv);
+    }
+    else if (seleccion == 2)
+    {
+        proceso = &Promedio;
+        (*proceso)(argc, argv);
+    }
+    else
+    {
+        cout << "You trash kid xdxdxdxd";
+        main(argc, argv);
+    }
+
     return 0;
 }
